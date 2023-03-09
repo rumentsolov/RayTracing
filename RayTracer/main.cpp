d70cac96f5250cc61e8cc8dfc842d1ba4320445b
Rumen Tsolov <r.tsolov86@gmail.com>
2023-03-09 09:56:47 +0200

    object matrix created!

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -5,35 +5,16 @@
 #include "generateCubes.h"
 
 
-class Matrix{
-
-    public:
-    Matrix(){
-        
-        std::string** str = new std::string*[iWidth];
-
-        for (int i = 0; i < iWidth; i++) {
-            str[i] = new std::string[iHeight]; // Declare a memory block of size n
-        }
-
-        for (int i = 0; i < iWidth; i++) {
-            for (int j = 0; j < iHeight; j++) {
-                str[i][j] = "- - -  ";
-            }
-        }
-
-        for (int i = 0; i < iWidth; i++) {
-            for (int j = 0; j < iHeight; j++) {
-                std::cout << str[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-};
-
-
 int main() {
+
+    Matrix matrix;
+
+
+    std::string changed = "9 9 9  ";
+
+    matrix.change(1 , 1 , 4 , 4, changed);
+
+    matrix.print();
 
 	openFile();
 
--- RayTracer/matrix.h
+++ RayTracer/matrix.h
@@ -4,5 +4,50 @@
 #include "publics.h"
 #include "getRand.h"
 
+class Matrix{
+
+public:
+
+    std::string** str;
+    
+    Matrix(){
+        
+        str = new std::string*[iWidth];
+
+        for (int i = 0; i < iWidth; i++) {
+            str[i] = new std::string[iHeight]; // Declare a memory block of size n
+        }
+
+        for (int i = 0; i < iWidth; i++) {
+            for (int j = 0; j < iHeight; j++) {
+                str[i][j] = "- - -  ";
+            }
+        }
+    }
+
+        void print(){
+            for (int i = 0; i < iWidth; i++) {
+                for (int j = 0; j < iHeight; j++) {
+                    std::cout << str[i][j] << " ";
+                }
+            std::cout << std::endl;
+            }
+        }
+        
+
+
+        void change(int startHeight, int startWigth , int endHeight, int endWigth, std::string & put){
+            for (int i = 0; i < iWidth; i++) {
+                for (int j = 0; j < iHeight; j++) {
+
+                if(i>=startHeight && j>=startWigth&&i<endHeight && j<endWigth)
+                    str[i][j] = put;
+                }
+            std::cout << std::endl;
+            }
+        }
+        
+
+};
 
 #endif
