dfeaf2c78b009c0b8c8c7506b808ac2f7c88bb85
rumentsolov <r.tsolov86@gmail.com>
2023-03-27 16:28:27 +0300

    new

--- Homework-4/main.cpp
+++ Homework-4/main.cpp
@@ -1,11 +1,14 @@
 #include "cameraBuff.h"
-#include "vec.h"
-
-//asdf
 
 int main() {
-
-    CameraBuff buff(1300,1400 );
+    /*
+    Optimizaions I have to make:
+    1) All object to be from type Vec ( Vertex || Point)
+    2) Image Buff can be allocated in single Vec instead of 2D array
+    3) PixelColor uses floats so size can be smaller ( unsigned char )
+    4) To check for unused operator overloading function
+    */
+    CameraBuff buff(620,550 );
     buff.record();
     
 	return 0;
